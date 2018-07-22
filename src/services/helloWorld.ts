d747ca48e6a0cc139c4052a3a327f379fd5d6fbd
Michael D. Norman <michael.norman@denormans.com>
2018-07-22 15:54:08 -0500

    better error handling

--- src/services/helloWorld.ts
+++ src/services/helloWorld.ts
@@ -1,6 +1,14 @@
-export const helloWorld = async (word?: String) => {
+import { ArgumentError } from '../utils/errors';
+
+class IllegalWordError extends ArgumentError {
+  constructor(word: string) {
+    super(`Bad word given: '${word}'`);
+  }
+}
+
+export const helloWorld = async (word?: string) => {
   if (word === 'BAD!') {
-    throw Error('Bad word given');
+    throw new IllegalWordError(word);
   }
 
   return word ? `Hello ${word}!` : 'Hello Lambda World!';
