e6d6a913caf7841f0e61c230f6fee38abff83f77
Michael D. Norman <michael.norman@denormans.com>
2018-07-22 15:36:35 -0500

    test error

--- src/events/helloWorld.ts
+++ src/events/helloWorld.ts
@@ -1,26 +1,4 @@
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, Context } from 'aws-lambda';
 import { helloWorld } from '../services/helloWorld';
 import { createApiGatewayProxyHandler } from './apiGatewayProxyHandlerCreator';
-
-export interface HelloRequest {
-  word?: string;
-}
-
-export interface HelloResponse {
-  statusCode: number;
-  body: string;
-}
-
-export const handler2: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context) => {
-  console.log('Event:', JSON.stringify(event));
-  console.log('Context:', JSON.stringify(context));
-
-  const params = event.queryStringParameters || (event.body && JSON.parse(event.body)) || {};
-  const body = await helloWorld(params.word);
-  return {
-    body,
-    statusCode: 200,
-  };
-};
 
 export const handler = createApiGatewayProxyHandler(({ word }) => helloWorld(word));
--- src/services/helloWorld.ts
+++ src/services/helloWorld.ts
@@ -1,1 +1,7 @@
-export const helloWorld = async (word?: String) => (word ? `Hello ${word}!` : 'Hello Lambda World!');
+export const helloWorld = async (word?: String) => {
+  if (word === 'BAD!') {
+    throw Error('Bad word given');
+  }
+
+  return word ? `Hello ${word}!` : 'Hello Lambda World!';
+};
